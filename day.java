fcf7713e64922cf718c2fedbc68b89859d95c1d2
Venkatesh <venk01atesh@gmail.com>
2023-08-14 21:28:26 +0530

    m

---
+++ day.java
@@ -0,0 +1,11 @@
+package yash;
+import java.util.Arrays;
+ class day{
+    public static void main(String[] args) {
+        int[] marks = {11, 22, 55,};
+        int[][] finalmarks = {{11, 22, 55},{44, 55, 66}};
+        System.out.println(finalmarks[1][2]);
+
+
+    }
+ }
